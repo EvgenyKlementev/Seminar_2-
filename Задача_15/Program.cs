c621d5f46012e45adcda31ba06e00b0aea498be0
unknown <klementyev.ev@gmail.com>
2023-02-12 23:46:20 +0300

    Задача 15

---
+++ Задача_15/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день 
+недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Введите число: ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int Saturday=6;
+int Sunday=7;
+
+if(number==Saturday||number==Sunday)
+Console.WriteLine("Поздравляю, у Вас выходной!");
+else Console.WriteLine("Идем на работу!");
